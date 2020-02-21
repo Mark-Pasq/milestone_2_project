bdc984506489a97755b0924be55f08993a4bcecf
Mark P Pasquantonio Sr <mpasquantoni@student.bridgew.edu>
2020-02-21 08:12:03 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-
-
 from utils import database
 
 USER_CHOICE = """
@@ -14,47 +12,47 @@ Your choice:"""
 
 
 def menu():
+    database.create_book_table()
     user_input = input(USER_CHOICE)
     while user_input != 'q':
         if user_input == 'a':
-            prompt_add_book()
+            prompt_add_book_to_the_list()
         elif user_input == 'l':
             list_all_books()
         elif user_input == 'r':
             prompt_mark_book_as_read()
         elif user_input == 'd':
-            prompt_delete_book()
+            prompt_to_delete_a_book()
         else:
             print('Unknown command or input.  Try your selection again.')
 
         user_input = input(USER_CHOICE)
 
 
-def prompt_add_book():
-    name = input('Enter the name of the new book: ')
-    author = input('Enter the name of the author of the new book: ')
+def prompt_add_book_to_the_list():
+    name = input(f'Enter the name of the new book:  ')
+    author = input(f'Enter the name of the author of the new book:  ')
 
-    database.add_book_to_list(name, author)
+    database.prompt_add_book_to_the_list(name, author)
 
 
 def list_all_books():
-    books = database.get_all_books()
+    books = database.list_all_books()
     for book in books:
-        read = 'YES' if book['read'] == '1' else 'NO'
+        read = 'YES' if book['read'] else 'NO'
         print(f"{book['name']} by {book['author']}, read: {book['read']}")
 
 
 def prompt_mark_book_as_read():
-    name = input('Enter the name of the book you have just finished reading: ')
+    name = input(f'Enter the name of the book you have just finished reading: ')
 
     database.prompt_mark_book_as_read(name)
 
 
-def prompt_delete_book():
-    name = input('Enter the name of the book you would like to delete from the list: ')
+def prompt_to_delete_a_book():
+    name = input(f'Enter the name of the book you would like to delete from the list: ')
 
-    database.delete_book(name)
+    database.prompt_to_delete_a_book(name)
 
 
 menu()
-
--- utils/database.py
+++ utils/database.py
@@ -4,45 +4,57 @@ Format of the csv file:
 
 name, author, read\n
 """
-books_file = 'books.txt'
+
+import sqlite3
 
 
-def add_book_to_list(name, author):
-    with open('books.txt', 'a') as file:
-        file.write(f'{name}, {author},0')
+def create_book_table():
+    conn = sqlite3.connect('data.db')
+    cu = conn.cursor()
+
+    cu.execute(f'''CREATE TABLE IF NOT EXISTS books(name TEXT PRIMARY KEY, author TEXT, read_status INTEGER); ''')
+
+    conn.commit()
+    conn.close()
 
 
-def get_all_books():
-    with open(books_file, 'r') as file:
-        lines = [line.strip().split(',') for line in file.readlines()]
+def prompt_add_book_to_the_list(name, author):
+    conn = sqlite3.connect('data.db')
+    cu = conn.cursor()
 
-    return [
-        {'name': line[0], 'author': line[1], 'read': line[2]}
-        for line in lines
-    ]
+    cu.execute(f'''INSERT INTO books VALUES(?, ?, 0)''', (name, author))
+
+    conn.commit()
+    conn.close()
+
+
+def list_all_books():
+    conn = sqlite3.connect('data.db')
+    cu = conn.cursor()
+
+    cu.execute(f'''SELECT * FROM books ''')
+    books = [{'name': row[0], 'author': row[1], 'read_status': row[2]} for row in cu.fetchall()]
+
+    conn.close()
+
+    return books
 
 
 def prompt_mark_book_as_read(name):
-    books = get_all_books()
-    for book in books:
-        if book['name'] == name:
-            book['read'] = '1'
-        _save_all_books(books)
+    conn = sqlite3.connect('data.db')
+    cu = conn.cursor()
+
+    cu.execute('''UPDATE books SET read=1 WHERE name=?, (name, )''')
+
+    cu.commit()
+    cu.close()
 
 
-def _save_all_books(books):
-    with open(books_file, 'w') as file:
-        for book in books:
-            file.write(f"{book['name']}, {book['author']}, {book['read']}\n")
+def prompt_to_delete_a_book(name):
+    conn = sqlite3.connect('data.db')
+    cu = conn.cursor()
 
+    cu.execute('''DELETE FROM books WHERE name=?, (name, )''')
 
-
-def delete_book(name):
-    books = get_all_books()
-    books = [book for book in books if book['name'] != name]
-    _save_all_books(books)
-
-# def delete_book(name):
-#     for book in books:
-#         if book['name'] == name:
-#             books.remove(book)
+    cu.commit()
+    cu.close()
